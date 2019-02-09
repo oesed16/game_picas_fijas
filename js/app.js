0abd33c2482509af871d73b7f5a3992264ec128c
Angela Castagno <oesed16@gmail.com>
2019-02-09 16:51:27 -0500

    Versión preliminar del proyecto picas y fijas

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var template = Handlebars.compile($('#num-template').html());
+
+var arrayUniqueNum = getArrayUniqueNum();
+var uniqueNum = arrayUniqueNum.join('');
+console.log(uniqueNum);
+var correct = uniqueNum;
+
+$('#input').keypress(function (e) {
+    var valueInput = $(this).val();
+    var only_num = /^(?!.*(.).*\1)\d{4}$/;
+    var is_num = only_num.test(valueInput);
+    // Validación para que el input no esté en blanco, tenga 4 carateres y sean números
+    if (e.which === 13) {
+        if (is_num) {
+            var arrayAnsUser = Array.from(valueInput);
+            var countPicas = 0;
+            var countFijas = 0;
+            $.each(arrayAnsUser, function (index, value) {
+                if ($.inArray(value, correct) !== -1) {
+                    countPicas = countPicas + 1;
+                }
+                if (correct[index] === arrayAnsUser[index]) {
+                    countFijas = countFijas + 1;
+                }
+            });
+            var dataFromUser = {
+                numUser: valueInput,
+                picas: countPicas,
+                fijas: countFijas
+            }
+            $(this).val('');
+            $(this).removeClass("invalid");
+            $('p span').removeClass("error");
+            $('table tbody:first').after().prepend(template(dataFromUser));
+        } else {
+            $(this).addClass("invalid");
+            $('p span').addClass("error");
+        }
+    }
+    if (valueInput === correct) {
+        $('.ganaste').show();
+    }
+});
+
+$('.close a').on('click', function () {
+    $('.ganaste').hide();
+});
+
+function getArrayUniqueNum() {
+    var arrayUniqueNum = [];
+    return Array.from({ length: 4 }, () => {
+        var randomValue = Math.floor((Math.random() * 9));
+        while (arrayUniqueNum.includes(randomValue)) {
+            randomValue = Math.floor((Math.random() * 9));
+        }
+        arrayUniqueNum.push(randomValue);
+        return randomValue;
+    });
+}
